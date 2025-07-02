f46bb4d77241f8f793334e26f407dae6fa6efdd6
Huberthol <100531060+Huberthol@users.noreply.github.com>
2025-07-02 10:57:01 +0200

    Update conftest.py

--- .github/workflows/GithubactionsTest/tests/conftest.py
+++ .github/workflows/GithubactionsTest/tests/conftest.py
@@ -1,9 +1,15 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
-from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="chrome", help="chrome or firefox")
+    parser.addoption("--headless", action="store_true", help="Enable headless mode")
 
 @pytest.fixture
 def driver(request):
@@ -13,17 +19,17 @@ def driver(request):
     if browser == "chrome":
         options = ChromeOptions()
         if headless:
-            options.add_argument("--headless")
+            options.add_argument("--headless=new")
             options.add_argument("--no-sandbox")
             options.add_argument("--disable-dev-shm-usage")
-        service = ChromeService()  # domyślnie używa chromedriver z PATH
+        service = ChromeService(ChromeDriverManager().install())
         driver = webdriver.Chrome(service=service, options=options)
 
     elif browser == "firefox":
         options = FirefoxOptions()
         if headless:
             options.headless = True
-        service = FirefoxService()
+        service = FirefoxService(GeckoDriverManager().install())
         driver = webdriver.Firefox(service=service, options=options)
 
     else:
